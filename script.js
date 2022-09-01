1b7f4ff032d707bfcaed5c63c5a90f7a206ec8b3
Johan Perez <johan.perez05@gmail.com>
2022-09-01 16:15:31 +0200

    roll dice, current display and change player added

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const btnAdd = document.querySelector('#roll-dice');
+const btnRoll = document.querySelector('#roll-dice');
 
+let player = 1;
 let random;
+let current = document.querySelector(`#current${player}`);
+let currentNumb = 0;
 
-btnAdd.addEventListener('click', () => {
-    random = Math.floor(Math.random() * 6 + 1);
+function playerTurn() {
+  if (player == 1) {
+    player = 2;
+  } else player = 1;
+};
+
+btnRoll.addEventListener('click', () => {
+  random = Math.floor(Math.random() * 6 + 1);
+  if (random == 1) {
+    currentNumb = 0;
+    playerTurn();
+  } else currentNumb += random;
+  current.innerHTML = currentNumb;
+
+  current = document.querySelector(`#current${player}`);
 });
+
