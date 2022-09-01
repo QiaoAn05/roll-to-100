bfd2b13b84e1b0fcecda8d293c83033d13e9c20d
Johan Perez <johan.perez05@gmail.com>
2022-09-01 17:39:02 +0200

    current.innerHTML added to the btnRoll to display 0 if random == 1

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const btnRoll = document.querySelector('#roll-dice');
 const btnHold = document.querySelector('#hold');
+const btnNewGame = document.querySelector('#new-game');
 
-let player = 1;
+let player;
 let random;
 let current = document.querySelector(`#current${player}`);
 let score = document.querySelector(`#score${player}`);
-let currentNumb = 0;
-let scoreNumb1 = 0;
-let scoreNumb2 = 0;
+let currentNumb;
+let scoreNumb1;
+let scoreNumb2;
+
+newGame()
+
+function newGame() {
+    player = 1;
+    current = document.querySelector(`#current${player}`);
+    score = document.querySelector(`#score${player}`);
+    currentNumb = 0;
+    scoreNumb1 = 0;
+    scoreNumb2 = 0;
+};
 
 function playerTurn() {
   if (player == 1) {
     player = 2;
-  } else player = 1;
+    current = document.querySelector(`#current${player}`);
+    score = document.querySelector(`#score${player}`);
+  } else if (player ==2) {
+    player = 1;
+    current = document.querySelector(`#current${player}`);
+    score = document.querySelector(`#score${player}`);
+  };
 };
 
 btnRoll.addEventListener('click', () => {
   random = Math.floor(Math.random() * 6 + 1);
   if (random == 1) {
     currentNumb = 0;
+    current.innerHTML = currentNumb;
     playerTurn();
   } else if (random != 1) {
     currentNumb += random;
     current.innerHTML = currentNumb;
   }; 
 
-  current = document.querySelector(`#current${player}`);
-  score = document.querySelector(`#score${player}`);
+  
 });
 
 btnHold.addEventListener('click', () => {
@@ -45,3 +63,6 @@ btnHold.addEventListener('click', () => {
     
 });
 
+btnNewGame.addEventListener('click', () => {
+    newGame();
+});
