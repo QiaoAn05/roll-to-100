1bc9b9fcb78b9248251100ed33aaaf45d6b0c17c
Johan Perez <johan.perez05@gmail.com>
2022-09-01 17:12:25 +0200

    add hold button

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const btnRoll = document.querySelector('#roll-dice');
+const btnHold = document.querySelector('#hold');
 
 let player = 1;
 let random;
 let current = document.querySelector(`#current${player}`);
+let score = document.querySelector(`#score${player}`);
 let currentNumb = 0;
+let scoreNumb1 = 0;
+let scoreNumb2 = 0;
 
 function playerTurn() {
   if (player == 1) {
@@ -16,9 +20,27 @@ btnRoll.addEventListener('click', () => {
   if (random == 1) {
     currentNumb = 0;
     playerTurn();
-  } else currentNumb += random;
-  current.innerHTML = currentNumb;
+  } else if (random != 1) {
+    currentNumb += random;
+    current.innerHTML = currentNumb;
+  }; 
 
   current = document.querySelector(`#current${player}`);
+});
+
+btnHold.addEventListener('click', () => {
+    if (player == 1) {
+        scoreNumb1 += currentNumb;
+        score.innerHTML = scoreNumb1;
+    } else if (player == 2) {
+        scoreNumb2 += currentNumb;
+        score.innerHTML = scoreNumb2;
+    };
+    currentNumb = 0;
+    current.innerHTML = currentNumb;
+    playerTurn();
+    current = document.querySelector(`#current${player}`);
+    score = document.querySelector(`#score${player}`);
+    
 });
 
